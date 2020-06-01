84dd927510c5e9157d10e75709b6ef3e587f24a0
1ayuu <46105942+1ayuu@users.noreply.github.com>
2020-06-01 16:09:07 +0545

    Create MovementThrowKeyboard.cs

---
+++ BasicBox/movementpart/MovementThrowKeyboard.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementThrowKeyboard: MonoBehaviour
+{
+    public float xspeed=0.01f;
+    public float yspeed=0.02f;
+ 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // print("this works!");
+    }
+
+    // Update is called once per frame
+    void Update()
+   {
+           Move();
+    }
+    void Move()
+    {     if (Input.GetKey(KeyCode.D))
+            {
+                transform.position +=new Vector3(xspeed,0,0);
+                }
+
+                    if (Input.GetKey(KeyCode.A))
+                {
+                    transform.position -=new Vector3(xspeed,0,0);
+                    }
+      
+                    if (Input.GetKey(KeyCode.W))
+                {
+                    transform.position +=new Vector3(0,yspeed,0);
+                    }
+                        if (Input.GetKey(KeyCode.S))
+                {
+                    transform.position -=new Vector3(0,yspeed,0);
+                    }
+
+    }
+    
